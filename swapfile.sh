623da0eb91cf635ace542898432587ac2536ee8b
Crypto Noodle <36743992+Cryptonoodle@users.noreply.github.com>
2018-03-14 01:06:39 -0700

    Update swapfile.sh
    Added missing line, nano..

--- swapfile.sh
+++ swapfile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# create swap file in bytes; increments of 1024(1gb),2048(2gb),etc. 
 SWAP_SIZE_MEGABYTES=2048
 if [ $SWAP_SIZE_MEGABYTES -eq 0 ];then
         echo No swap size given, skipping.
@@ -8,7 +7,7 @@ else
                 echo /swapfile already exists, skiping.
         else
                 echo Creating /swapfile of $SWAP_SIZE_MEGABYTES Megabytes
-                dd if=/dev/zero of=/swapfile bs=1024 count=$(($SWAP_SIZE_MEGABY$
+                dd if=/dev/zero of=/swapfile bs=1024 count=$(($SWAP_SIZE_MEGABYTES*1024))
                 mkswap /swapfile
                 swapon /swapfile
                 echo Swap Status:
